1fae4c6c023573d69e81301ac0f86332495b2c1e
benckx <benoit.vleminckx@restore.eu>
2021-09-10 09:00:17 +0200

    run remote command

---
+++ src/main/kotlin/be/encelade/vaporwave/Client.kt
@@ -0,0 +1,49 @@
+package be.encelade.vaporwave
+
+import com.jcraft.jsch.ChannelExec
+import com.jcraft.jsch.JSch
+import com.jcraft.jsch.Session
+import java.io.ByteArrayOutputStream
+
+class Client(private val username: String,
+             private val password: String,
+             private val host: String,
+             private val port: Int = 22) {
+
+    fun isOnline(): Boolean {
+        return try {
+            val session = JSch().getSession(username, host, port)
+            session.setPassword(password)
+            session.setConfig("StrictHostKeyChecking", "no")
+            session.connect()
+            session.disconnect()
+            true
+        } catch (t: Throwable) {
+            false
+        }
+    }
+
+    fun sendCommand(command: String): String {
+        var session: Session? = null
+        var channel: ChannelExec? = null
+        try {
+            session = JSch().getSession(username, host, port)
+            session.setPassword(password)
+            session.setConfig("StrictHostKeyChecking", "no")
+            session.connect() // send Exception if offline
+            channel = session.openChannel("exec") as ChannelExec
+            channel.setCommand(command)
+            val responseStream = ByteArrayOutputStream()
+            channel.outputStream = responseStream
+            channel.connect()
+            while (channel.isConnected) {
+                Thread.sleep(100)
+            }
+            return String(responseStream.toByteArray())
+        } finally {
+            session?.disconnect()
+            channel?.disconnect()
+        }
+    }
+
+}
--- src/main/kotlin/be/encelade/vaporwave/Main.kt
+++ src/main/kotlin/be/encelade/vaporwave/Main.kt
@@ -1,35 +1,13 @@
 package be.encelade.vaporwave
 
-import com.jcraft.jsch.ChannelExec
-import com.jcraft.jsch.JSch
-import com.jcraft.jsch.Session
-import java.io.ByteArrayOutputStream
-
 fun main() {
-    val command = " ls -l --time-style=full-iso /roms/*/*.{srm,state}"
-    val result = sendCommand("ark", "ark", "192.168.178.57", 22, command)
-    println(result)
-}
-
-fun sendCommand(username: String, password: String, host: String, port: Int, command: String): String {
-    var session: Session? = null
-    var channel: ChannelExec? = null
-    try {
-        session = JSch().getSession(username, host, port)
-        session.setPassword(password)
-        session.setConfig("StrictHostKeyChecking", "no")
-        session.connect() // send Exception if offline
-        channel = session.openChannel("exec") as ChannelExec
-        channel.setCommand(command)
-        val responseStream = ByteArrayOutputStream()
-        channel.outputStream = responseStream
-        channel.connect()
-        while (channel.isConnected) {
-            Thread.sleep(100)
-        }
-        return String(responseStream.toByteArray())
-    } finally {
-        session?.disconnect()
-        channel?.disconnect()
+    val client = Client("ark", "ark", "192.168.178.57", 22)
+    if (client.isOnline()) {
+//        val command = " ls -l --time-style=full-iso /roms/*/*.{srm,state}"
+        val command = " ls -l --time-style=full-iso /roms/*/*.*"
+        val result = client.sendCommand(command)
+        println(result)
+    } else {
+        println("offline")
     }
 }

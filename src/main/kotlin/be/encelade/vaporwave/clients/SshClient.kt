5b1621707d6d491c0b869c5bbb70ba8f9dee5622
benckx <benoit.vleminckx@restore.eu>
2021-09-16 12:17:54 +0200

    refactored ssh client

--- src/main/kotlin/be/encelade/vaporwave/clients/SshClient.kt
+++ src/main/kotlin/be/encelade/vaporwave/clients/SshClient.kt
@@ -2,12 +2,10 @@ package be.encelade.vaporwave.clients
 
 import be.encelade.vaporwave.model.devices.SshConnection
 import be.encelade.vaporwave.utils.LazyLogging
-import com.jcraft.jsch.ChannelExec
-import com.jcraft.jsch.ChannelSftp
-import com.jcraft.jsch.JSch
-import com.jcraft.jsch.Session
+import com.jcraft.jsch.*
 import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.FileInputStream
 
 internal class SshClient(private val username: String,
                          private val password: String,
@@ -18,50 +16,36 @@ internal class SshClient(private val username: String,
 
     fun isReachable(): Boolean {
         return try {
-            val session = JSch().getSession(username, host, port)
-            session.setPassword(password)
-            session.setConfig("StrictHostKeyChecking", "no")
-            session.connect()
+            val session = buildSession()
+            session.connect() // send Exception if offline
             session.disconnect()
             true
         } catch (t: Throwable) {
+            logger.error("failed to open session: " + t.message)
             false
         }
     }
 
     fun sendCommand(command: String): String {
-        var session: Session? = null
-        var channel: ChannelExec? = null
-        try {
-            session = JSch().getSession(username, host, port)
-            session.setPassword(password)
-            session.setConfig("StrictHostKeyChecking", "no")
-            session.connect() // send Exception if offline
-            channel = session.openChannel("exec") as ChannelExec
+        val responseStream = ByteArrayOutputStream()
+
+        openExecChannel { channel ->
             channel.setCommand(command)
-            val responseStream = ByteArrayOutputStream()
             channel.outputStream = responseStream
             channel.connect()
             while (channel.isConnected) {
                 Thread.sleep(100)
             }
-            return String(responseStream.toByteArray())
-        } finally {
-            session?.disconnect()
-            channel?.disconnect()
         }
+
+        responseStream.close()
+        return String(responseStream.toByteArray())
     }
 
     fun downloadFiles(filePaths: List<String>, targetFolder: String): List<File> {
         val result = mutableListOf<File>()
-        var session: Session? = null
-        var channel: ChannelSftp? = null
-        try {
-            session = JSch().getSession(username, host, port)
-            session.setPassword(password)
-            session.setConfig("StrictHostKeyChecking", "no")
-            session.connect() // send Exception if offline
-            channel = session.openChannel("sftp") as ChannelSftp
+
+        openSftpChannel { channel ->
             filePaths.forEach { filePath ->
                 val fileName = filePath.split("/").last()
                 val targetFilePath = "$targetFolder${File.separator}/$fileName"
@@ -69,11 +53,48 @@ internal class SshClient(private val username: String,
                 channel.get(filePath, targetFilePath)
                 result += File(targetFilePath)
             }
+        }
+
+        return result
+    }
+
+    fun uploadFiles(files: List<Pair<File, String>>) {
+        openSftpChannel { channel ->
+            files.forEach { (file, targetFilePath) ->
+                logger.debug("uploading ${file.absolutePath} to $targetFilePath...")
+                channel.put(FileInputStream(file), targetFilePath)
+            }
+        }
+    }
+
+    private fun openExecChannel(block: (ChannelExec) -> Unit) {
+        openChannel("exec", block)
+    }
+
+    private fun openSftpChannel(block: (ChannelSftp) -> Unit) {
+        openChannel("sftp", block)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <C : Channel> openChannel(channelType: String, block: (C) -> Unit) {
+        var session: Session? = null
+        var channel: C? = null
+        try {
+            session = buildSession()
+            session.connect()
+            channel = session.openChannel(channelType) as C
+            block(channel)
         } finally {
             session?.disconnect()
             channel?.disconnect()
         }
-        return result
+    }
+
+    private fun buildSession(): Session {
+        val session = JSch().getSession(username, host, port)
+        session.setPassword(password)
+        session.setConfig("StrictHostKeyChecking", "no")
+        return session
     }
 
 }
